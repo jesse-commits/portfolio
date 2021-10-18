c5bca43ee7c2c390ef679e83f55cb76b26139bef
Jesse <jessehglvn@gmail.com>
2021-10-18 00:15:44 -0400

    completed styling the single page app with static text

--- app/assets/scripts/modules/Time.js
+++ app/assets/scripts/modules/Time.js
@@ -30,16 +30,16 @@ class Time {
     day = checkTime(day)
     
     let ourTime = `<article class="time__article">
-                        <h1>${h}:${m}:${s}</h1>
-                        <span>${d}</span>
-                        <span> ${mo} ${day-1}, ${y}</span>
+                        <h1 class="general-content--large">${h}:${m}:${s}</h1>
+                        <span class="general-content--dark">${d}</span>
+                        <span class="general-content--dark"> ${mo} ${day-1}, ${y}</span>
                     </article>`
     
         time.innerHTML = ourTime
 
-    let that = this
+    let me = this
 
-    let t = setTimeout(function(){that.getTime()},500)
+    let t = setTimeout(function(){me.getTime()},500)
     
  }
 
